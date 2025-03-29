53f0fa54fff00859fbf2f4462f6c74f13e236e75
Divyansh Singhal <divyansh.singhal@paloaltosoft.com>
2025-03-29 21:25:43 +0530

    2

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-3xl font-bold text-green-500">
-     hello
+     hello 123
     </div>
   );
 }
